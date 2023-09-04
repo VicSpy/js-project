13f4fc36afe1ffabd8cba7b7674025a8a5c82b6d
Victor Kruger <98160719+VicSpy@users.noreply.github.com>
2023-09-04 17:04:35 -0300

    Initial Commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const listaDeTeclas = document.querySelectorAll('.tecla'); 
+
+function tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+listaDeTeclas[0].onclick = tocaSomPom;
